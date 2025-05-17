a395b5efe09aae8b16f6e34ab5484dabd6120fa9
sabapailodze04 <sabapailodze2004@gmail.com>
2025-05-17 03:21:29 -0700

    core/featura
    making html structure
    adding script

---
+++ script.js
@@ -0,0 +1,22 @@
+// ! saba pailodze.  17.05.25. //
+// variables let & const//
+let firstname = "Saba";
+const lastname = "Pailodze";
+let email = "sabapailodze2004@gmail.com";
+let number = 568876542;
+let age = 21;
+let hobby = "Dancing";
+const isStudent = true;
+// changed variables//
+hobby = "reading";
+age = age + 1;
+// valiue section//
+console.log("First Name: " + firstname);
+console.log("Last Name: " + lastname);
+console.log("Email: " + email);
+console.log("Number: " + number);
+console.log("Age: " + age);
+console.log("Hobby: " + hobby);
+// updated valiue section//
+console.log("Updated age: " + age);
+console.log("Updated hobby: " + hobby);
